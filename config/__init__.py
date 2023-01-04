00daba283ddc0e0fb136d33423c9f5e01b334786
Salman Adhikari <80933048+SalmanAd01@users.noreply.github.com>
2023-01-04 16:02:01 +0530

    Revert "Lint"

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,8 @@
 from dotenv import load_dotenv
+load_dotenv('.env')
+from routes.index import index
+from . import firebase
 
-
-load_dotenv(".env")
+def create_app(app):
+    app.register_blueprint(index,url_prefix='/')
+    return app
