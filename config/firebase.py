a71f95dbe0a605732da6e51eb9046002af7bedad
Salman Adhikari <80933048+SalmanAd01@users.noreply.github.com>
2023-01-04 16:03:44 +0530

    Revert "Revert "Lint""

--- config/firebase.py
+++ config/firebase.py
@@ -1,16 +1,17 @@
 import os
 import json
 import firebase_admin
-from firebase_admin import credentials,storage
+from firebase_admin import credentials
 
 dirname = os.path.dirname(__file__)
-file = os.path.join(dirname, 'arena-recomendation-firebase.json')
+file = os.path.join(dirname, "arena-recomendation-firebase.json")
 
-data = json.load(open(file))
-data['private_key'] = os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n')
+with open(file, encoding="utf-8") as f:
+    data = json.load(f)
+data["private_key"] = os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n")
 
 cred = credentials.Certificate(data)
 
-firebase_admin.initialize_app(cred, {
-    'storageBucket': os.environ.get('FIREBASE_STORAGE_BUCKET')
-})
+firebase_admin.initialize_app(
+    cred, {"storageBucket": os.environ.get("FIREBASE_STORAGE_BUCKET")}
+)
